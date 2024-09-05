9501eaca852285d5a0b5564493795a765a39982e
Evangelista Nobre <38170234+evannobre@users.noreply.github.com>
2024-09-04 21:54:47 -0300

    Alterações nas variáveis
    Adição da ID e mudança de uns nomes

--- 02 - Lógica de Programação/01 - Projeto Final (Lista de Supermercado)/assets/script.js
+++ 02 - Lógica de Programação/01 - Projeto Final (Lista de Supermercado)/assets/script.js
@@ -1,10 +1,15 @@
-let tasks = [];
+let produtos = [];
+let counter = 0;
 
 const ordemAlfabetica = (elementosDoSelect) => { 
   let elementos = Array.from(elementosDoSelect.options);
   elementos.sort((a, b) => a.text.localeCompare(b.text));
   elementosDoSelect.innerHTML = "";
   elementos.forEach(item => elementosDoSelect.add(item));
+}
+
+function idsSequenciais() {
+  return counter++;
 }
 
 const elementosDoSelect = document.getElementById('categoria');
@@ -18,12 +23,14 @@ document
     event.preventDefault(); // Evita o recarregamento da página por causa do submit do form
 
     // Obtendo os valores inseridos no formulário
+    let id = idsSequenciais();
     const produto = document.getElementById("produto").value;
     const quantidade = document.getElementById("quantidade").value;
     const categoria = document.getElementById("categoria").value;
 
     // adicionando os valores na lista de tarefas para printar no console
-    tasks.push({
+    produtos.push({
+      id,
       produto,
       quantidade,
       categoria,
@@ -36,13 +43,15 @@ document
     const newRow = table.insertRow();
 
     // Adicionando as células na nova linha
-    const produtoCell = newRow.insertCell(0);
-    const quantidadeCell = newRow.insertCell(1);
-    const categoriaCell = newRow.insertCell(2);
-    const editCell = newRow.insertCell(3);
-    const deleteCell = newRow.insertCell(4);
+    const idCell = newRow.insertCell(0);
+    const produtoCell = newRow.insertCell(1);
+    const quantidadeCell = newRow.insertCell(2);
+    const categoriaCell = newRow.insertCell(3);
+    const editCell = newRow.insertCell(4);
+    const deleteCell = newRow.insertCell(5);
 
     // Inserindo os valores nas células
+    idCell.textContent = id;
     produtoCell.textContent = produto;
     quantidadeCell.textContent = quantidade;
     categoriaCell.textContent = categoria;
@@ -52,5 +61,5 @@ document
     // Limpando o formulário após adicionar a tarefa
     document.getElementById("supermarketForm").reset();
 
-    console.log({ tasks });
+    console.log({ produtos });
   });
