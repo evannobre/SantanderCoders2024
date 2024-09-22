7aff57a575e788f743594b0945c52d8eed198cc7
Evangelista Nobre <38170234+evannobre@users.noreply.github.com>
2024-09-22 16:22:50 -0300

    Adição das funções para somar, subtrair, multiplicar e dividir
    Código feito com arrows functions e prompts, sem interface

--- 04 - Empregabilidade/Calculadora/script.js
+++ 04 - Empregabilidade/Calculadora/script.js
@@ -1,2 +1,6 @@
-const operacao = prompt("Digite o número da operação escolhida: \n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão")
+const operacao = prompt("Digite o número da operação escolhida: \n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão");
 
+const somarNumeros = (a, b) => a + b;
+const subtrairNumeros = (a, b) => a - b;
+const multiplicarNumeros = (a, b) => a * b;
+const dividirNumeros = (a, b) => a / b;
