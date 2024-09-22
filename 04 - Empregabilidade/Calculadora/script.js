fc0320b17c21d7163e699b3a3e1da42be5e696e4
Evangelista Nobre <38170234+evannobre@users.noreply.github.com>
2024-09-22 16:26:58 -0300

    Calculadora com prompts
    Adição dos prompts para o usuário entrar com os número, bem como o switch-case para controle das operações

--- 04 - Empregabilidade/Calculadora/script.js
+++ 04 - Empregabilidade/Calculadora/script.js
@@ -1,6 +1,19 @@
 const operacao = prompt("Digite o número da operação escolhida: \n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão");
+const numeroA = prompt("Digite um número: \nPara decimais, use '.' ao invés de vírgula");
+const numeroB = prompt("Digite outro número: \nPara decimais, use '.' ao invés de vírgula");
 
 const somarNumeros = (a, b) => a + b;
 const subtrairNumeros = (a, b) => a - b;
 const multiplicarNumeros = (a, b) => a * b;
 const dividirNumeros = (a, b) => a / b;
+
+switch(operacao) {
+    case '1':
+        return somarNumeros(numeroA, numeroB);
+    case '2':
+        return subtrairNumeros(numeroA, numeroB);
+    case '3':
+        return multiplicarNumeros(numeroA, numeroB);
+    case '4':
+        return dividirNumeros(numeroA, numeroB);
+}
