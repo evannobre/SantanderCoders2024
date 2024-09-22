a071c323816f1ceca736b44ff76578e287abe241
Evangelista Nobre <38170234+evannobre@users.noreply.github.com>
2024-09-22 16:28:15 -0300

    Conversão string-float
    Convertendo as entradas numéricas em float, para que a calculadora funcione

--- 04 - Empregabilidade/Calculadora/script.js
+++ 04 - Empregabilidade/Calculadora/script.js
@@ -1,6 +1,6 @@
 const operacao = prompt("Digite o número da operação escolhida: \n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão");
-const numeroA = prompt("Digite um número: \nPara decimais, use '.' ao invés de vírgula");
-const numeroB = prompt("Digite outro número: \nPara decimais, use '.' ao invés de vírgula");
+const numeroA = parseFloat(prompt("Digite um número: \nPara decimais, use '.' ao invés de vírgula"));
+const numeroB = parseFloat(prompt("Digite outro número: \nPara decimais, use '.' ao invés de vírgula"));
 
 const somarNumeros = (a, b) => a + b;
 const subtrairNumeros = (a, b) => a - b;
